d28a8520ed62326c4285783558668a5a4dec9f2c
H.P. Grahsl <grahslhp@gmail.com>
2022-07-30 15:34:36 +0200

    pre-register commonly found classes in backwards compatible order while keeping registration required false for now

--- connect-transform-kryptonite/src/main/java/com/github/hpgrahsl/kafka/connect/transforms/kryptonite/serdes/KryoInstance.java
+++ connect-transform-kryptonite/src/main/java/com/github/hpgrahsl/kafka/connect/transforms/kryptonite/serdes/KryoInstance.java
@@ -17,29 +17,69 @@
 package com.github.hpgrahsl.kafka.connect.transforms.kryptonite.serdes;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.util.DefaultInstantiatorStrategy;
 import com.github.hpgrahsl.kafka.connect.transforms.kryptonite.serdes.KryoSerdeProcessor.SchemaSerializer;
 import com.github.hpgrahsl.kafka.connect.transforms.kryptonite.serdes.KryoSerdeProcessor.StructSerializer;
 import com.github.hpgrahsl.kryptonite.EncryptedField;
 import com.github.hpgrahsl.kryptonite.FieldMetaData;
 import com.github.hpgrahsl.kryptonite.PayloadMetaData;
+import de.javakaffee.kryoserializers.SynchronizedCollectionsSerializer;
+import de.javakaffee.kryoserializers.UnmodifiableCollectionsSerializer;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.Struct;
+import org.objenesis.strategy.StdInstantiatorStrategy;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.*;
 
 public class KryoInstance {
 
   private static final Kryo KRYO = new Kryo();
 
   static {
-    KRYO.setRegistrationRequired(false);
-    //TODO: register all commonly found types for more efficient serialization
-    //...
-    KRYO.register(FieldMetaData.class);
-    KRYO.register(PayloadMetaData.class);
-    KRYO.register(EncryptedField.class);
-
-    //NOTE: needed in order to be able to serialize structs with their schemas
-    KRYO.register(Struct.class).setSerializer(new StructSerializer());
-    KRYO.register(Schema.class).setSerializer(new SchemaSerializer());
+    try {
+      KRYO.setWarnUnregisteredClasses(true);
+      KRYO.setRegistrationRequired(false);
+      KRYO.setInstantiatorStrategy(new DefaultInstantiatorStrategy(new StdInstantiatorStrategy()));
+      //NOTE: pre-register kryptonite for kafka specific classes and if applicable the necessary custom serializers
+      KRYO.register(FieldMetaData.class);
+      KRYO.register(PayloadMetaData.class);
+      KRYO.register(EncryptedField.class);
+      KRYO.register(Struct.class).setSerializer(new StructSerializer());
+      KRYO.register(Schema.class).setSerializer(new SchemaSerializer());
+      KRYO.register(Schema.Type.class);
+      //NOTE: pre-registering a couple of commonly found classes
+      //      in the context of kafka connect and ksqlDB
+      KRYO.register(Object.class);
+      KRYO.register(byte[].class);
+      KRYO.register(BigDecimal.class);
+      KRYO.register(List.class);
+      KRYO.register(ArrayList.class);
+      KRYO.register(LinkedList.class);
+      KRYO.register(Map.class);
+      KRYO.register(HashMap.class);
+      KRYO.register(LinkedHashMap.class);
+      KRYO.register(Set.class);
+      KRYO.register(HashSet.class);
+      KRYO.register(LinkedHashSet.class);
+      KRYO.register(Date.class);
+      KRYO.register(Time.class);
+      KRYO.register(Timestamp.class);
+      KRYO.register(Class.forName("java.util.Arrays$ArrayList"));
+      KRYO.register(Class.forName("java.util.ImmutableCollections$ListN"));
+      KRYO.register(Class.forName("java.util.ImmutableCollections$List12"));
+      KRYO.register(Class.forName("java.util.ImmutableCollections$SetN"));
+      KRYO.register(Class.forName("java.util.ImmutableCollections$Map1"));
+      KRYO.register(Class.forName("java.util.ImmutableCollections$MapN"));
+      //NOTE: kryo community serializers for other specific collection types
+      UnmodifiableCollectionsSerializer.registerSerializers(KRYO);
+      SynchronizedCollectionsSerializer.registerSerializers(KRYO);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public static Kryo get() {
