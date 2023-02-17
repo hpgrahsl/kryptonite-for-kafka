938b060338189555618e9466a3faefa26e98859d
H.P. Grahsl <grahslhp@gmail.com>
2023-02-17 22:24:29 +0100

    support multi-threaded usage by making threads own their own instance of kryo
    version bumping for patch release across modules

--- kryptonite/src/main/java/com/github/hpgrahsl/kryptonite/serdes/KryoInstance.java
+++ kryptonite/src/main/java/com/github/hpgrahsl/kryptonite/serdes/KryoInstance.java
@@ -35,53 +35,56 @@ import java.util.*;
 
 public class KryoInstance {
 
-  private static final Kryo KRYO = new Kryo();
-
-  static {
-    try {
-      KRYO.setWarnUnregisteredClasses(true);
-      KRYO.setRegistrationRequired(false);
-      KRYO.setInstantiatorStrategy(new DefaultInstantiatorStrategy(new StdInstantiatorStrategy()));
-      //NOTE: pre-register kryptonite for kafka specific classes and if applicable the necessary custom serializers
-      KRYO.register(FieldMetaData.class);
-      KRYO.register(PayloadMetaData.class);
-      KRYO.register(EncryptedField.class);
-      KRYO.register(Struct.class).setSerializer(new KryoSerdeProcessor.StructSerializer());
-      KRYO.register(Schema.class).setSerializer(new KryoSerdeProcessor.SchemaSerializer());
-      KRYO.register(Schema.Type.class);
-      //NOTE: pre-registering a couple of commonly found classes
-      //      in the context of kafka connect and ksqlDB
-      KRYO.register(Object.class);
-      KRYO.register(byte[].class);
-      KRYO.register(BigDecimal.class);
-      KRYO.register(List.class);
-      KRYO.register(ArrayList.class);
-      KRYO.register(LinkedList.class);
-      KRYO.register(Map.class);
-      KRYO.register(HashMap.class);
-      KRYO.register(LinkedHashMap.class);
-      KRYO.register(Set.class);
-      KRYO.register(HashSet.class);
-      KRYO.register(LinkedHashSet.class);
-      KRYO.register(Date.class);
-      KRYO.register(Time.class);
-      KRYO.register(Timestamp.class);
-      KRYO.register(Class.forName("java.util.Arrays$ArrayList"));
-      KRYO.register(Class.forName("java.util.ImmutableCollections$ListN"));
-      KRYO.register(Class.forName("java.util.ImmutableCollections$List12"));
-      KRYO.register(Class.forName("java.util.ImmutableCollections$SetN"));
-      KRYO.register(Class.forName("java.util.ImmutableCollections$Map1"));
-      KRYO.register(Class.forName("java.util.ImmutableCollections$MapN"));
-      //NOTE: kryo community serializers for other specific collection types
-      UnmodifiableCollectionsSerializer.registerSerializers(KRYO);
-      SynchronizedCollectionsSerializer.registerSerializers(KRYO);
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-  }
+  private static final ThreadLocal<Kryo> KRYOS = new ThreadLocal<Kryo>() {
+    protected Kryo initialValue() {
+      Kryo kryo = new Kryo();
+      try {
+        kryo.setWarnUnregisteredClasses(true);
+        kryo.setRegistrationRequired(false);
+        kryo.setInstantiatorStrategy(new DefaultInstantiatorStrategy(new StdInstantiatorStrategy()));
+        // NOTE: pre-register kryptonite for kafka specific classes and if applicable
+        // the necessary custom serializers
+        kryo.register(FieldMetaData.class);
+        kryo.register(PayloadMetaData.class);
+        kryo.register(EncryptedField.class);
+        kryo.register(Struct.class).setSerializer(new KryoSerdeProcessor.StructSerializer());
+        kryo.register(Schema.class).setSerializer(new KryoSerdeProcessor.SchemaSerializer());
+        kryo.register(Schema.Type.class);
+        // NOTE: pre-registering a couple of commonly found classes
+        // in the context of kafka connect and ksqlDB
+        kryo.register(Object.class);
+        kryo.register(byte[].class);
+        kryo.register(BigDecimal.class);
+        kryo.register(List.class);
+        kryo.register(ArrayList.class);
+        kryo.register(LinkedList.class);
+        kryo.register(Map.class);
+        kryo.register(HashMap.class);
+        kryo.register(LinkedHashMap.class);
+        kryo.register(Set.class);
+        kryo.register(HashSet.class);
+        kryo.register(LinkedHashSet.class);
+        kryo.register(Date.class);
+        kryo.register(Time.class);
+        kryo.register(Timestamp.class);
+        kryo.register(Class.forName("java.util.Arrays$ArrayList"));
+        kryo.register(Class.forName("java.util.ImmutableCollections$ListN"));
+        kryo.register(Class.forName("java.util.ImmutableCollections$List12"));
+        kryo.register(Class.forName("java.util.ImmutableCollections$SetN"));
+        kryo.register(Class.forName("java.util.ImmutableCollections$Map1"));
+        kryo.register(Class.forName("java.util.ImmutableCollections$MapN"));
+        // NOTE: kryo community serializers for other specific collection types
+        UnmodifiableCollectionsSerializer.registerSerializers(kryo);
+        SynchronizedCollectionsSerializer.registerSerializers(kryo);
+        return kryo;
+      } catch (ClassNotFoundException e) {
+        throw new RuntimeException(e);
+      }
+    };
+  };
 
   public static Kryo get() {
-    return KRYO;
+    return KRYOS.get();
   }
 
 }
